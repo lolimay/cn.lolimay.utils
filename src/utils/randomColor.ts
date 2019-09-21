ea803088611378303872316a63fcfc730c545fa3
lolimay <lolimay@lolimay.cn>
2019-09-22 04:53:06 +0800

    Add more unit tests

--- src/utils/randomColor.ts
+++ src/utils/randomColor.ts
@@ -1,6 +1,26 @@
 /**
- * Generate a random hex format color string.
+ * Generate a random color, hex string format by default.
+ * @param type specify the color type, hex string by default.
  */
-export function randomColor(): string {
-    return `#${ Math.round((Math.random() * 0xffffff)).toString(16).padStart(6, '0')}`;
+export function randomColor(type: ColorType = ColorType.HEX): string {
+    const hexColorString = `${ Math.round((Math.random() * 0xffffff)).toString(16).padStart(6, '0')}`;
+
+    if (!Object.values(ColorType).includes(<ColorType>type.toUpperCase())) {
+        throw new Error('The type parameter passed in is unknown!');
+    }
+
+    if (<ColorType>type.toUpperCase() === ColorType.RGB) {
+        const red = parseInt(hexColorString.slice(0, 1), 16).toString().padStart(3, '0');
+        const green = parseInt(hexColorString.slice(2, 3), 16).toString().padStart(3, '0');
+        const blue = parseInt(hexColorString.slice(4, 5), 16).toString().padStart(3, '0');
+
+        return(`rgb(${ red },${ green },${ blue })`);
+    }
+
+    return `#${ hexColorString }`;
+}
+
+export enum ColorType {
+    RGB = 'RGB',
+    HEX = 'HEX'
 }
--- tests/randomColor.spec.ts
+++ tests/randomColor.spec.ts
@@ -1,6 +1,12 @@
-import { randomColor } from '../src/utils/randomColor';
+import { randomColor, ColorType } from '../src/utils/randomColor';
 
 const REPEAT_TIMES = 10000;
+
+interface IColor {
+    red: number,
+    green: number,
+    blue: number
+}
 
 test('the length of returned hex color string should be 7', () => {
     const results = [];
@@ -28,3 +34,57 @@ test('should obey a uniform random distribution', () => {
     
     expect(frequency > 0.48 && frequency < 0.52).toBe(true);
 });
+
+test('should throw an error when pass a wrong color type', () => {
+    expect(() => {
+        // @ts-ignore
+        randomColor('')
+        // @ts-ignore
+        randomColor('xxss')
+    }).toThrowError();
+});
+
+test('should not throw an error when pass a right color type', () => {
+    expect(() => {
+        // @ts-ignore
+        randomColor('rgb')
+        // @ts-ignore
+        randomColor('hex')
+        // @ts-ignore
+        randomColor('hEx')
+    }).not.toThrowError();
+});
+
+test('rgb result should be in the right range', () => {
+    const results: Array<IColor> = [];
+
+    for (let i=0; i<REPEAT_TIMES; i++) {
+        const rgbColor: string = randomColor(ColorType.RGB);
+        const parsedResult: RegExpMatchArray | null = rgbColor.match(/rgb\((\d{0,3}),(\d{0,3}),(\d{0,3})\)/);
+        
+        expect(parsedResult).not.toBe(null);
+
+        if (parsedResult !== null) {
+            const [, red, green, blue] = parsedResult;
+            expect(
+                red.length === green.length
+                && green.length === blue.length
+                && blue.length === 3
+            ).toBe(true);
+
+            results.push({
+                red: parseInt(red),
+                green: parseInt(green),
+                blue: parseInt(blue)
+            });
+        }
+    }
+
+    results.forEach((color: IColor) => {
+        const { red, green, blue } = color;
+
+        expect(red >= 0 && red <= 255).toBe(true);
+        expect(green >= 0 && green <= 255).toBe(true);
+        expect(blue >= 0 && blue <= 255).toBe(true);
+    });
+})
